cfc7ad279119e93037c71e90b48a94a4d55413a5
weishuaishuai <2683409132@qq.com>
2024-05-23 11:34:27 +0800

    初始化xiaobudong项目

---
+++ src/main/java/com/xiaobudong/xiaobudong/XiaobudongApplication.java
@@ -0,0 +1,13 @@
+package com.xiaobudong.xiaobudong;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XiaobudongApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(XiaobudongApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/xiaobudong/xiaobudong/XiaobudongApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xiaobudong.xiaobudong;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XiaobudongApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
